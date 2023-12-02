9a80129081a0934077c0d186e3d1aad4d5481b6e
Presley Sakwa <presleyingolo@gamil.com>
2023-12-02 01:02:38 -0800

    changed requirements,txt

--- orders/views.py
+++ orders/views.py
@@ -2,52 +2,205 @@ from basket.basket import Basket
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
+from django.template.loader import render_to_string
 from .models import Order, OrderItem
+from decimal import Decimal
+from django.shortcuts import render
 
 import os
 
 
+# def add(request):
+#     basket = Basket(request)
+#     if request.POST.get("action") == "post":
+
+#         order_key = request.POST.get("order_key")
+#         user_id = request.user.id
+#         baskettotal = basket.get_total_price()
+
+#         # Check if order exists
+#         if Order.objects.filter(order_key=order_key).exists():
+#             pass
+#         else:
+#             order = Order.objects.create(
+#                 user_id=user_id,
+#                 full_name="name",
+#                 address1="add1",
+#                 address2="add2",
+#                 total_paid=baskettotal,
+#                 order_key=order_key,
+#             )
+#             order_id = order.pk
+
+#             for item in basket:
+#                 OrderItem.objects.create(
+#                     order_id=order_id,
+#                     product=item["product"],
+#                     price=item["price"],
+#                     quantity=item["qty"],
+#                 )
+
+#         # response = JsonResponse({"success": "Return something"})
+#         # return response
+#         return HttpResponseRedirect('/order_placed.html/')
+#     return HttpResponseRedirect('/order_placed.html/')
+
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from decimal import Decimal
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView
+
+
+
+from django.shortcuts import get_object_or_404
+
+
+
+#############################good#################################################
+
+
+# def add(request):
+#     basket = Basket(request)
+#     if request.method == 'POST':
+#         order_key = request.POST.get("order_key")
+#         user_id = request.user.id
+
+#         try:
+#             total_paid_string = basket.get_total_price()
+#             total_paid_string = 11
+#             total_paid = Decimal(total_paid_string)
+#         except decimal.InvalidOperation:
+#             error_message = "Invalid total_paid value: {}".format(total_paid_string)
+#             return redirect('/order_error.html?error={}'.format(error_message))
+
+#         existing_order = Order.objects.filter(order_key=order_key).first()
+#         if existing_order:
+#             existing_order.total_paid = total_paid
+#             existing_order.save()
+
+#             # Get or create an OrderItem object with the ID of 1
+#             product, created = OrderItem.objects.get_or_create(pk=1)
+
+#             # Set the price field for the OrderItem object before trying to save it
+#             product.price = product.product.price
+
+#             order_item = OrderItem.objects.filter(order=existing_order, product=product).first()
+
+#             if order_item:
+#                 order_item.quantity += 1
+#                 order_item.save()
+#             else:
+#                 OrderItem.objects.create(order=existing_order, product=product, price=product.price, quantity=1)
+
+#             return redirect('/order_placed.html/')
+#         else:
+#             order = Order.objects.create(user_id=user_id, full_name="name", address1="add1", address2="add2", total_paid=total_paid, order_key=order_key)
+#             order_id = order.pk
+
+#             # Get or create an OrderItem object with the ID of 1
+#             product, created = OrderItem.objects.get_or_create(pk=1)
+
+#             # Set the price field for the OrderItem object before trying to save it
+#             product.price = product.product.price
+
+#             OrderItem.objects.create(order_id=order_id, product=product, price=product.price, quantity=1)
+
+#             return redirect('/order_placed.html/')
+#     else:
+#         return render(request, 'add_order.html', {'basket': basket})
+##################################END#######################################################
+from django.shortcuts import render
+
+
+# def add(request):
+#     basket = Basket(request)
+
+#     if request.method == 'POST':
+#         order_key = request.POST.get("order_key")
+#         user_id = request.user.id
+
+#         try:
+#             # total_paid_string = basket.get_total_price()
+#             total_paid_string = 11
+#             total_paid = Decimal(total_paid_string)
+#         except decimal.InvalidOperation:
+#             error_message = "Invalid total_paid value: {}".format(total_paid_string)
+#             return redirect('/order_error.html?error={}'.format(error_message))
+
+#         existing_order = Order.objects.filter(order_key=order_key).first()
+
+#         if existing_order:
+#             # Order already exists, no need to create OrderItem object
+#             existing_order.total_paid = total_paid
+#             existing_order.save()
+#         else:
+#             # Order does not exist, create OrderItem object
+#             order = Order.objects.create(user_id=user_id, full_name="name", address1="add1", address2="add2", total_paid=total_paid, order_key=order_key)
+#             order_id = order.pk
+
+#             # Get or create an OrderItem object with the ID of 1
+#             product, created = OrderItem.objects.get_or_create(pk=1)
+
+#             # Explicitly set the price field for the OrderItem object before saving
+#             product.price = product.product.price
+#             product.save()
+
+#         return render(request, 'order_placed.html', {})
+#     else:
+#         return render(request, 'order_placed.html', {})
+
+
+###############################################second############################
+
 def add(request):
     basket = Basket(request)
-    if request.POST.get("action") == "post":
 
+    if request.method == 'POST':
         order_key = request.POST.get("order_key")
         user_id = request.user.id
-        baskettotal = basket.get_total_price()
 
-        # Check if order exists
-        if Order.objects.filter(order_key=order_key).exists():
-            pass
+        try:
+            total_paid_string = 10
+            total_paid = Decimal(total_paid_string)
+        except decimal.InvalidOperation:
+            error_message = "Invalid total_paid value: {}".format(total_paid_string)
+            # return redirect('/order_error.html?error={}'.format(error_message))
+            return render(request, 'order_placed.html', {})
+
+        existing_order = Order.objects.filter(order_key=order_key).first()
+
+        if existing_order:
+            # Order already exists, no need to create OrderItem object
+            existing_order.total_paid = total_paid
+            existing_order.save()
         else:
-            order = Order.objects.create(
-                user_id=user_id,
-                full_name="name",
-                address1="add1",
-                address2="add2",
-                total_paid=baskettotal,
-                order_key=order_key,
-            )
-            order_id = order.pk
+            # Order does not exist, create OrderItem object
+            # order = Order.objects.create(user_id=user_id, full_name="name", address1="add1", address2="add2", total_paid=total_paid, order_key=order_key)
+            # order_id = order.pk
 
-            for item in basket:
-                OrderItem.objects.create(
-                    order_id=order_id,
-                    product=item["product"],
-                    price=item["price"],
-                    quantity=item["qty"],
-                )
+            # Hardcode price for testing
+            product_price = 10.00  # Replace with actual price retrieval logic for production
 
-        # response = JsonResponse({"success": "Return something"})
-        # return response
-        return HttpResponseRedirect('/order_placed.html/')
-    return HttpResponseRedirect('/order_placed.html/')
+            # Get or create an OrderItem object with the ID of 1
+            # product, created = OrderItem.objects.get_or_create(pk=1)
+
+            # Set the price field for the OrderItem object
+            # product.price = product_price
+            # product.save()
+
+        return render(request, 'order_placed.html', {})
+    else:
+        return render(request, 'order_placed.html', {})
 
 
 
 
 def payment_confirmation(data):
-    Order.objects.filter(order_key=data).update(is_delivered=True)
+    # Order.objects.filter(order_key=data).update(is_delivered=True)
+    pass
 
 
 def user_orders(request):
--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -6,81 +6,104 @@ clientsecret = elem.getAttribute('data-secret');
 // Set up Stripe.js and Elements to use in checkout form
 var elements = stripe.elements();
 var style = {
-  base: {
-    color: "#000",
-    lineHeight: '2.4',
-    fontSize: '16px'
-  }
+    base: {
+        color: "#000",
+        lineHeight: '2.4',
+        fontSize: '16px'
+    }
 };
+
 
 var card = elements.create("card", { style: style });
 card.mount("#card-element");
 
 card.on('change', function (event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-    $('#card-errors').addClass('alert alert-info');
-  } else {
-    displayError.textContent = '';
-    $('#card-errors').removeClass('alert alert-info');
-  }
+    var displayError = document.getElementById('card-errors')
+    if (event.error) {
+        displayError.textContent = event.error.message;
+        $('#card-errors').addClass('alert alert-info');
+    } else {
+        displayError.textContent = '';
+        $('#card-errors').removeClass('alert alert-info');
+    }
 });
 
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function (ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 
-  var custName = document.getElementById("custName").value;
-  var custAdd = document.getElementById("custAdd").value;
-  var custAdd2 = document.getElementById("custAdd2").value;
-  var postCode = document.getElementById("postCode").value;
+    var custName = document.getElementById("custName").value;
+    var custAdd = document.getElementById("custAdd").value;
+    var custAdd2 = document.getElementById("custAdd2").value;
+    var postCode = document.getElementById("postCode").value;
 
-  $.ajax({
-    type: "POST",
-    url: 'http://127.0.0.1:8000/orders/add/',
-    data: {
-      order_key: clientsecret,
-      csrfmiddlewaretoken: CSRF_TOKEN,
-      action: "post",
-    },
-    success: function (json) {
-      console.log(json.success);
+    const clientSecret = '{{ payment_intent.client_secret }}';
+    console.log(clientSecret);
 
-      stripe.confirmCardPayment(clientsecret, {
+    try {
+      stripe.confirmCardPayment(clientSecret, {
         payment_method: {
-          card: card,
-          billing_details: {
-            address: {
-              line1: custAdd,
-              line2: custAdd2
-            },
-            name: custName
+          card: {
+            token: '{{ token }}'
           }
         }
-      }).then(function (result) {
-        if (result.error) {
-          console.log('payment error');
-          console.log(result.error.message);
-
-          // Render error message to the order_placed.html template
-          $.get('/payment/templates/order_placed.html', function (template) {
-            $('#payment-form').html(template);
-            $('#payment-form').prepend('<div class="alert alert-danger">Payment failed: ' + result.error.message + '</div>');
-          });
-        } else {
-          if (result.paymentIntent.status === 'succeeded') {
-            console.log('payment processed');
-
-            // Redirect to order_placed.html template upon successful payment
-            window.location.replace("/payment/templates/order_placed.html");
-          }
-        }
+      }).then(function(result) {
+        // Payment confirmed
+      }).catch(function(error) {
+        // Payment failed
       });
-    },
-    error: function (xhr, errmsg, err) {
-      console.error('AJAX error:', errmsg, err);
+    } catch (error) {
+      console.error('Error confirming payment:', error);
     }
-  });
-});
+    
+
+      
+
+
+//     $.ajax({
+//         type: "POST",
+//         url: 'http://127.0.0.1:8000/payment/templates/order_placed.html',
+//         // http://127.0.0.1:5500/payment/templates/order_placed.html
+//         data: {
+//             order_key: clientsecret,
+//             csrfmiddlewaretoken: CSRF_TOKEN,
+//             action: "post",
+//         },
+//         success: function (json) {
+//             console.log(json.success)
+
+//             stripe.confirmCardPayment(clientsecret, {
+//                 payment_method: {
+//                     card: card,
+//                     billing_details: {
+//                         address: {
+//                             line1: custAdd,
+//                             line2: custAdd2
+//                         },
+//                         name: custName
+//                     },
+//                 }
+//             }).then(function (result) {
+//                 if (result.error) {
+//                     console.log('payment error')
+//                     console.log(result.error.message);
+//                 } else {
+//                     if (result.paymentIntent.status === 'succeeded') {
+//                         console.log('payment processed')
+//                         // There's a risk of the customer closing the window before callback
+//                         // execution. Set up a webhook or plugin to listen for the
+//                         // payment_intent.succeeded event that handles any business critical
+//                         // post-payment actions.
+//                         window.location.replace("http://127.0.0.1:5500/orderplaced/");
+//                     }
+//                 }
+//             });
+
+//         },
+//         error: function (xhr, errmsg, err) { },
+//     });
+
+
+
+// });
