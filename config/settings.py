db13476a33614fd1eb8251f74e15f759d40aaab0
Presley Sakwa <presleyingolo@gamil.com>
2023-12-02 02:50:17 -0800

    changed config

--- config/settings.py
+++ config/settings.py
@@ -113,6 +113,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+django_heroku.settings(locals())
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
